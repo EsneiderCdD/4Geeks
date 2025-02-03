61a5f6693fdb4fe53b8e3d54d708f6ffbd431919
AnaliaMagallanes <analiapiriwindy@gmail.com>
2025-02-02 21:38:24 -0300

    Agregados Calificaciones-TituloSlider

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,10 +4,14 @@ import Inicio from '../components/Inicio'
 import Slider from '../components/Slider'
 import Calificaciones from '../components/Calificaciones'
 import Acordeon from '../components/Acordeon'
+import Presentacion from '../components/Presentacion'
+import TituloSlider from '../components/TituloSlider'
 const Home = () => {
   return (
     <div>
+     <Presentacion/>
      <Carrusel/>
+     <TituloSlider/>
      <Slider/>
      <Acordeon/>
      <Calificaciones/>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
 
-const Slider = () => {
+import { useState, useEffect } from "react";
+
+const images = [
+  { src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK_nmxFKUd1XKM0QyNZh4h6j3UJYQKIXmveg&s", title: "Imagen 1" },
+  { src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyDJ-wd9gTsrWHd9C3ccWmh-wMKTCQMuWn2w&s", title: "Imagen 2" },
+];
+
+export default function Slider() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <h1>
-        Slider
-    
-      </h1>
+    <div className="relative flex justify-center items-center h-64 w-full bg-[#f0e6d2] overflow-hidden">
+      {images.map((img, i) => (
+        <div
+          key={i}
+          className={`absolute w-64 h-64 rounded-full flex flex-col justify-center items-center bg-[#d3c0b1] shadow-lg transition-all duration-1000 ${
+            i === index ? "opacity-100 scale-100" : "opacity-0 scale-50"
+          }`}
+          style={{
+            transform: `translate(${i === index ? "0, 0" : "200%, 200%"}) rotate(${i === index ? "0deg" : "180deg"})`,
+          }}
+        >
+          <img src={img.src} alt={img.title} className="w-48 h-48 rounded-full" />
+          <p className="mt-2 text-lg text-[#6f4f37] font-semibold">{img.title}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
-
-export default Slider
-

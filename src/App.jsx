57767097aa6850694238d6489e8b251f2e1641be
ecd.97@hotmail.com <ecd.97@hotmail.com>
2025-02-14 19:22:03 -0500

    Componente: Comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Login from './components/Login';
 import Register from './components/Register';
 import DeleteUser from './components/DeleteUser';
 import UpdateUser from './components/UpdateUser';
+import Logout from './components/Logout';
+import Comments from './components/Comments';
+
 
 
 
@@ -14,8 +17,9 @@ function App() {
             <Register />
             <DeleteUser />
             <UpdateUser />
-
-
+            <Logout />
+            <Comments />
+          
         </div>
     );
 }

097730a386ba8df0e1179bbdafddf3ace03efc2a
ecd.97@hotmail.com <ecd.97@hotmail.com>
2025-02-15 14:36:24 -0500

    Ruteando los primeros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Register from './components/Register';
-import DeleteUser from './components/DeleteUser';
 import UpdateUser from './components/UpdateUser';
+import DeleteUser from './components/DeleteUser';
 import Logout from './components/Logout';
-import Comments from './components/Comments';
 
-
-
-
-function App() {
-    return (
-        <div>
-            
-            <Login />
-            <Register />
-            <DeleteUser />
-            <UpdateUser />
-            <Logout />
-            <Comments />
-          
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/terapia-1-1" element={<div style={{textAlign: 'center', marginTop: '50px'}}>Página de Terapia 1:1</div>} />
+        <Route path="/terapia-grupal" element={<div style={{textAlign: 'center', marginTop: '50px'}}>Página de Terapia Grupal</div>} />
+        <Route path="/edit-user" element={<UpdateUser />} />
+        <Route path="/delete-user" element={<DeleteUser />} />
+        <Route path="/logout" element={<Logout />} />
+        
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;

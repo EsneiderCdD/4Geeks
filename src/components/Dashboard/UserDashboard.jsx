208d53e2cbef41e86413bf8f89193350a93dbe84
ecd.97@hotmail.com <ecd.97@hotmail.com>
2025-03-02 08:32:04 -0500

    Nuevo Contraataque

--- src/components/Dashboard/UserDashboard.jsx
+++ src/components/Dashboard/UserDashboard.jsx
@@ -1,20 +1,16 @@
 import "../../styles/UserDashboard.css";
-import UserNavBar from "./UserNavBar.jsx";
-import UserOptions from "./UserOptions.jsx";
-import DeleteUserButton from "../DeleteUserButton.jsx";
-import ModifyUser from "../ModifyUser.jsx";
+import NavBar from "../Navbar/NavBar";
 
 
 const UserDashboard = () => {
   return (
-    <div className="user-dashboard-background">
-        <UserNavBar></UserNavBar>
-      <div className="user-dashboard-sheet">
-        <UserOptions />
-        <DeleteUserButton />
-        <ModifyUser />
+    <div >
+      <NavBar/>
+      
+      
+      
        
-      </div>
+      
     </div>
   );
 };
